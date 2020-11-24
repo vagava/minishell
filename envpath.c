a1f9caf454eb005bb34100fd31c88b78448a605c
afrancocedeno <afrancocedeno@gmail.com>
2020-11-24 15:56:36 -0500

    00 | file create | seudo cod

--- envpath.c
+++ envpath.c
@@ -1,4 +1,9 @@
 int main(void)
 {
-	strtok( = )
+/* identificar PATH en doble puntero **env */
+/* tokenizar con "=" PATH = __rutas__ */
+/* identificar segundo token */
+/* tokenizar string resultante con : separando rutas */
+/* identificar la ruta solicitada */
+return (0);
 }
